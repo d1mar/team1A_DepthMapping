fbd3723681e3ce14183df906d821ee552fad569c
ubu_pi <pi@example.com>
2020-01-16 12:05:04 -0800

    need to copy paste before using kinect

---
+++ libfreenect/build/src/CMakeFiles/freenectstatic.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/pi1a/workspace/team1A_DepthMapping/libfreenect/src/audio.c" "/home/pi1a/workspace/team1A_DepthMapping/libfreenect/build/src/CMakeFiles/freenectstatic.dir/audio.c.o"
+  "/home/pi1a/workspace/team1A_DepthMapping/libfreenect/src/cameras.c" "/home/pi1a/workspace/team1A_DepthMapping/libfreenect/build/src/CMakeFiles/freenectstatic.dir/cameras.c.o"
+  "/home/pi1a/workspace/team1A_DepthMapping/libfreenect/src/core.c" "/home/pi1a/workspace/team1A_DepthMapping/libfreenect/build/src/CMakeFiles/freenectstatic.dir/core.c.o"
+  "/home/pi1a/workspace/team1A_DepthMapping/libfreenect/src/flags.c" "/home/pi1a/workspace/team1A_DepthMapping/libfreenect/build/src/CMakeFiles/freenectstatic.dir/flags.c.o"
+  "/home/pi1a/workspace/team1A_DepthMapping/libfreenect/src/loader.c" "/home/pi1a/workspace/team1A_DepthMapping/libfreenect/build/src/CMakeFiles/freenectstatic.dir/loader.c.o"
+  "/home/pi1a/workspace/team1A_DepthMapping/libfreenect/src/registration.c" "/home/pi1a/workspace/team1A_DepthMapping/libfreenect/build/src/CMakeFiles/freenectstatic.dir/registration.c.o"
+  "/home/pi1a/workspace/team1A_DepthMapping/libfreenect/src/tilt.c" "/home/pi1a/workspace/team1A_DepthMapping/libfreenect/build/src/CMakeFiles/freenectstatic.dir/tilt.c.o"
+  "/home/pi1a/workspace/team1A_DepthMapping/libfreenect/src/usb_libusb10.c" "/home/pi1a/workspace/team1A_DepthMapping/libfreenect/build/src/CMakeFiles/freenectstatic.dir/usb_libusb10.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "/usr/include/libusb-1.0"
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
