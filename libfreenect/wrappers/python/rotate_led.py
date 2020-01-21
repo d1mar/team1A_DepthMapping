d9548c85cb50250e31bf6627650ab8f74af957b8
ubu_pi <pi@example.com>
2020-01-21 12:25:41 -0800

    p2

--- libfreenect/wrappers/python/rotate_led.py
+++ libfreenect/wrappers/python/rotate_led.py
@@ -1,14 +1,34 @@
+#!/usr/bin/env python
 import freenect
 import time
 import signal
 
-led = 0
-tilt = 0
+keep_running = True
+last_time = 0
+led = -1
+tilt = -5
 def body(dev,ctx):
-   freenect.set_led(dev,led)
-   freenect.set_tilt_degs(dev,tilt)
-   print('led[%d] tilt[%d] accel[%s]' % (led,tilt,freenect.get_accel(dev)))
-   time.sleep(1)
-   led += 1
-   tilt +=5
+    global last_time
+    if not keep_running:
+        raise freenect.Kill
+    if time.time() - last_time < 3:
+        return
+    last_time = time.time()
+    led = led+1
+    tilt = tilt+5
+    freenect.set_led(dev, led)
+    freenect.set_tilt_degs(dev, tilt)
+    print('led[%d] tilt[%d] accel[%s]' % (led,tilt,freenect.get_accel(dev)))
+    time.sleep(1)
+    #led =  led+1
+    #tilt = tilt+5
+def handler(signum,frame):
+    global keep_running
+    keep_running = False
+
+#while True:
+#    body()
+#    if led == 6:
+#        break
+signal.signal(signal.SIGINT,handler)
 freenect.runloop(body=body)
