38be5c89c598fedd4289854968590d8bd8d1e3fb
d1mar <d1mar@ucsd.edu>
2020-02-11 23:11:10 -0800

    modifying sender/receiver files

--- p3_hardware/receiver/serial_connection_reciever.py
+++ p3_hardware/receiver/serial_connection_reciever.py
@@ -8,17 +8,17 @@ from neopixel import *
 import argparse
 
 # LED strip configuration:
-LED_COUNT      = 1000      # Number of LED pixels.
+LED_COUNT      = 216     # Number of LED pixels.
 LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
-#LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+#LED_PIN        = 10     # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
 LED_BRIGHTNESS = 100     # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
-CUBE_DIM       = 10      # define cube dimension
-LIT_LEDS       = 100     # number of LEDs lit at one time
-a
+CUBE_DIM       = 6       # define cube dimension
+LIT_LEDS       = 36      # number of LEDs lit at one time
+
 
 # Define functions which animate LEDs in various ways.
 def colorWipe(strip, color, wait_ms=0):
--- p3_hardware/sender/serial_connection_sender.py
+++ p3_hardware/sender/serial_connection_sender.py
@@ -1,8 +1,6 @@
 import serial
 import time
 import io
-#importing test.py which performs all computations
-import test
 
 ser = serial.Serial(
 	port = '/dev/serial0',
@@ -10,10 +8,7 @@ ser = serial.Serial(
 	bytesize = serial.EIGHTBITS,
 	timeout = 0)
 
-#running main in test.py will return filtered array
-str_list2 = test.main()
 str_list = [(0,0,0), (1,1,1), (2,2,2), (3,3,3), (4,4,4), (5,5,5), (6,6,6), (7,7,7), (8,8,8), (9,9,9)]
-
 
 '''
 The following line of code send every element of str_list
@@ -30,3 +25,15 @@ t = str.encode('\0')
 ser.write(t)
 
 ser.close()
+
+# function implemetation for integration into test.py
+# by doing this all that is left is making a call in test.py in order to send the array
+#def sender(string)
+#	for i in str_list:
+#		for j in i:
+#			#t = str(j) + '\0'
+#			t = str.encode(str(j))
+#			ser.write(t)
+#	t = str.encode('\0')
+#	ser.write(t)	
+#	ser.close()
